3b528c0862d57ac5006ed0a606118b8ccdd2e800
olvak <82042917+olvak@users.noreply.github.com>
2024-08-17 15:26:30 +0300

    replace the for with a while loop

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -3,12 +3,20 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-  return (
-	" ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber));
+  return (" ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber));
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+
+while (done !== count) {
+  done++;
+  rows.push(padRow(done, count));
 }
 
 let result = "";
