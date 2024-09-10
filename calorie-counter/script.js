9615357eadc7a79f63bc0726e74dcf260bf86fdc
olvak <82042917+olvak@users.noreply.github.com>
2024-09-10 17:15:02 +0300

    add cleanInputString()

---
+++ calorie-counter/script.js
@@ -0,0 +1,10 @@
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+let isError = false;
+
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+}
