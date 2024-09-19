dd93a526c142f07b0fbb9c3bf7483a4f5ef08d61
olvak <82042917+olvak@users.noreply.github.com>
2024-09-19 13:31:00 +0300

    add getRoundResults()

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -10,3 +10,17 @@ let computerScore = 0;
 function hasPlayerWonTheRound(player, computer) {
   return (player === "Rock" && computer === "Scissors") || (player === "Scissors" && computer === "Paper") || (player === "Paper" && computer === "Rock");
 }
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
