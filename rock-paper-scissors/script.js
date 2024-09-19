d0642638dc4d90e251bf0738713e9c7a4438a62a
olvak <82042917+olvak@users.noreply.github.com>
2024-09-19 11:40:29 +0300

    add hasPlayerWonTheRound()

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -3,3 +3,10 @@ function getRandomComputerResult() {
   const randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (player === "Rock" && computer === "Scissors") || (player === "Scissors" && computer === "Paper") || (player === "Paper" && computer === "Rock");
+}
